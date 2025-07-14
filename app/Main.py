699db6b26c1331adcdbfe99c57338be844a1d5e3
Carlos (Rick) <carloshenrique.chsl@outlook.com>
2025-07-14 02:02:37 -0300

    Creation of the

--- app/Main.py
+++ app/Main.py
@@ -5,9 +5,12 @@ print("Testando projeto")
 
 from Client import Client
 
+from Account import Account
+
 # a declaração de um novo objeto se dá
 # semelhantemente a declaração de uma variável
 c1=Client("João", "8199233-5678")
 
-print(c1)
-print(c1.name, "e" c1.telephone)
+account = Account(c1.name, 164521, 0)
+
+print("Holder: ", account.holder, "| Number: ", account.number, "| Balance: ",account.balance)
